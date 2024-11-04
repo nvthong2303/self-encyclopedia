312ac187c8de837b7cd1013b1f92f9a3a0f8c2b0
nvthong2303 <thong.nv1@cmctelecom.vn>
2024-11-04 10:22:20 +0700

    revert

--- Pythornhub/FastAPI-Clean-Example/models/BaseModel.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.ext.declarative import declarative_base
-
-from configs.Database import Engine
-
-EntityMeta = declarative_base()
-
-
-def init():
-    EntityMeta.metadata.create_all(bind=Engine)
